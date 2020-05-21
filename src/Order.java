c515d9e4d7a4ed87f806c9271b3b2b5c8e1f5fdd
bergelm <bergelm@gmail.com>
2020-05-21 11:48:58 +0200

    Initial commit

---
+++ src/Order.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Order {
+
+    private List<Dish> dishes;
+
+    public Order(List<Dish> dishes){
+        this.dishes = dishes;
+    }
+
+    public String toString(){
+        double dishesCost = PaymentService.calculateDishesSum(dishes);
+        double serviceCost = PaymentService.calculateServiceCost(dishesCost);
+        double totalCost = dishesCost + serviceCost;
+        return String.format("Do zapłaty: %.2f, w tym:\n" +
+                        "Koszt dań: %.2f \n" +
+                        "Koszt obsługi: %.2f",
+                totalCost, dishesCost, serviceCost);
+    }
+
+
+}
